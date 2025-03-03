504185f2b0ff9fb1e4867d8cf2b9f819a6ce5016
Sohaima <sohaimanasir1@gmail.com>
2025-03-03 10:03:16 +0500

    cc

--- Server01/index.js
+++ Server01/index.js
@@ -2,7 +2,7 @@ const http = require("http")
 const server = http.createServer((request,response)=>{
     
     response.writeHead(200, {"content-type":"text/plain"})
-    response.end("Server is working hello anusha")
+    response.end("Server is working ")
 
 
 
